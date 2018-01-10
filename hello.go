8829685f2089b4c9788a8023a0dee0a417243840
thanhluong <doannguyenthanhluong@gmail.com>
2018-01-10 19:12:11 +0700

    Testing POST method

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+    "fmt"
     "net/http"
     "github.com/gin-gonic/gin"
     "github.com/thinkerou/favicon"
@@ -10,7 +11,20 @@ func main() {
     r := gin.Default()
     r.Use(favicon.New("./img/fav.ico"))
     r.LoadHTMLGlob("templates/*")
-    r.GET("/:name", func(c *gin.Context){
+    r.POST("/postf", func(c *gin.Context){
+        value := c.PostForm("exval")
+        c.JSON(200, gin.H{
+            "result": "OK",
+            "value": value,
+        })
+        fmt.Printf("%s", value);
+    })
+    r.GET("/exform", func(c *gin.Context){
+        c.HTML(http.StatusOK, "exform.html", gin.H{
+            "title": "Testing form",
+        })
+    })
+    r.GET("/noform/:name", func(c *gin.Context){
         name := c.Param("name");
         c.HTML(http.StatusOK, "test.html", gin.H{
             "title": "Something",
