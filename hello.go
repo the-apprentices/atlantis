577673a4dcc1bf469049b97e64c4d2d46825c7d8
thanhluong <doannguyenthanhluong@gmail.com>
2018-01-10 11:47:49 +0700

    Test favicon

--- hello.go
+++ hello.go
@@ -3,14 +3,18 @@ package main
 import(
     "net/http"
     "github.com/gin-gonic/gin"
+    "github.com/thinkerou/favicon"
 )
 
 func main() {
     r := gin.Default()
+    r.Use(favicon.New("./img/fav.ico"))
     r.LoadHTMLGlob("templates/*")
-    r.GET("/index", func(c *gin.Context){
+    r.GET("/:name", func(c *gin.Context){
+        name := c.Param("name");
         c.HTML(http.StatusOK, "test.html", gin.H{
             "title": "Something",
+            "content": name,
         })
     })
     r.Run(":8080")
